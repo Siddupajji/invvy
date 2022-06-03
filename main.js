3daa08679e35056cf4d42e0ed2d0c70ac8e18ddb
Siddupajji <76541945+Siddupajji@users.noreply.github.com>
2022-06-03 14:39:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $(".slider-hero").slick({
+        dots: true,
+        horizontal: true,
+        centerMode: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+});
